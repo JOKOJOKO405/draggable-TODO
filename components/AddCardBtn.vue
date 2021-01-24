219b18a77bedf8ad3fb79f6637246e444667115f
JOKOJOKO405 <biz.kj405@gmail.com>
2021-01-24 16:11:45 +0900

    add カード追加ボタン

---
+++ components/AddCardBtn.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn fab color="primary" elevation="0" class="add-btn">
+    <v-icon large>mdi-plus-thick</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup(_, { root }) {}
+})
+</script>
+
+<style scoped>
+  .add-btn {
+    position: fixed;
+    bottom: 24px;
+    right: 24px;
+  }
+</style>
