cb36e5b4ac9c7a25e072e3c82e311c9cbf4e95df
JOKOJOKO405 <biz.kj405@gmail.com>
2021-01-24 11:46:54 +0900

    add テスト表示

---
+++ pages/login/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-card class="mx-auto pa-6">
+          あああ
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup (_, { root }) {
+    const test = 'aaa'
+
+    return {
+      test
+    }
+  }
+})
+</script>
