76676610c02d54310b78aa60efa711293e7552bc
JOKOJOKO405 <biz.kj405@gmail.com>
2021-02-02 21:00:16 +0900

    add カード追加フォーム

--- components/CardDialog.vue
+++ components/CardDialog.vue
@@ -1,21 +1,87 @@
 <template>
-  <v-row justify="center">
-    <v-dialog :value="isOpened" scrollable>
-      <v-card class="pa-8">
-        <v-row>
-          <v-spacer />
-          <v-icon class="gray400--text text-right" @click="cancelAction">
-            mdi-close-circle-outline
-          </v-icon>
-        </v-row>
-        <v-card-title
-          class="pa-0 primary--text font-weight-bold"
-        >
-          日付
+  <v-dialog :value="isOpened" scrollable max-width="600">
+    <v-card class="pa-8">
+      <v-row>
+        <v-spacer />
+        <v-icon large class="gray400--text text-right" @click="cancelAction">
+          mdi-close-circle-outline
+        </v-icon>
+      </v-row>
+      <v-form>
+        <v-card-title class="pa-0 primary--text font-weight-bold">
+          日時
         </v-card-title>
-      </v-card>
-    </v-dialog>
-  </v-row>
+        <v-row no-gutters class="mb-8">
+          <v-col cols="6">
+            <v-text-field
+              type="date"
+              class="pr-4 ma-0"
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              type="time"
+              class="pl-4 ma-0"
+            />
+          </v-col>
+        </v-row>
+        <v-card-title class="pa-0 primary--text font-weight-bold">
+          内容
+        </v-card-title>
+        <v-row no-gutters class="mb-8">
+          <v-col cols="12">
+            <v-text-field
+              type="text"
+              class="pa-0"
+            />
+            <v-chip
+              v-for="(chip) in demoChips"
+              :key="chip.id"
+              class="mr-4 primary--text"
+              color="#DAF9F1"
+            >
+              {{ chip.title }}
+            </v-chip>
+          </v-col>
+        </v-row>
+        <v-card-title class="pa-0 primary--text font-weight-bold mb-4">
+          カードの色
+        </v-card-title>
+        <v-row no-gutters class="mb-8">
+          <v-col cols="12">
+            <v-btn
+              v-for="(color, index) in cardColors"
+              :key="index"
+              class="mr-4"
+              fab
+              x-small
+              depressed
+              :style="color.style"
+              :color="color.name"
+            />
+          </v-col>
+        </v-row>
+        <v-card-title class="pa-0 primary--text font-weight-bold">
+          通知
+        </v-card-title>
+        <v-row no-gutters class="mb-8" align="center">
+          <v-col cols="2">
+            <v-text-field
+              type="number"
+              class="mr-4"
+            />
+          </v-col>
+          <v-col cols="2">
+            <v-select
+              :items="dates"
+              class="mr-4"
+            />
+          </v-col>
+          <v-col cols="2">前から</v-col>
+        </v-row>
+      </v-form>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script lang="ts">
@@ -34,6 +100,43 @@ export default defineComponent({
     okAction: {
       type: Function,
       default: () => {}
+    }
+  },
+  setup (_, { root }) {
+    // あとで消す
+    const demoChips = [
+      {
+        id: 1,
+        title: '在庫整理'
+      },
+      {
+        id: 2,
+        title: '電話'
+      },
+      {
+        id: 3,
+        title: '吉田さん'
+      }
+    ]
+    const cardColors = [
+      {name: 'white', style: 'border: 1px solid #BDBDBD !important;'},
+      {name: '#FFF9E4'},
+      {name: '#F4FFEC'},
+      {name: '#E9FFFA'},
+      {name: '#FFE9EE'},
+      {name: '#FFF3E9'},
+      {name: '#F7E9FF'}
+    ]
+
+    const dates = [
+      '日',
+      '時間'
+    ]
+
+    return {
+      demoChips,
+      cardColors,
+      dates
     }
   }
 })
