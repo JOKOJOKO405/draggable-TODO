39f0ab7f7a5db9eee1355bbc54ec43aafcd282b6
JOKOJOKO405 <biz.kj405@gmail.com>
2021-02-06 21:58:54 +0900

    add forで複数配置

--- components/Card.vue
+++ components/Card.vue
@@ -1,54 +1,56 @@
 <template>
-  <v-card id="card" class="pa-4 pt-2" :style="cardPosition">
-    <v-row no-gutters class="pa-0 pb-2">
-      <v-col
-        cols="9"
-        class="grabbingArea pa-0"
-        @mousedown="onMouseDown"
-        @mouseup="onMouseUp"
-        @dragstart="onDragStart"
-      /></v-col>
-      <v-col cols="3" class="px-0">
-        <v-btn small icon class="pa-0">
-          <v-icon class="text-right" color="gray400" @click="editCard"> mdi-pencil </v-icon>
-        </v-btn>
-        <v-btn small icon class="pa-0" @click="deleteCard">
-          <v-icon class="text-right" color="gray400"> mdi-close </v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-divider class="mb-2" />
-    <v-row no-gutters class="ma-0 pa-0">
-      <v-col cols="4" align-self="center">
-        <p class="date-time warning--text ma-0 font-weight-bold">
-          {{ date }}
-        </p>
-      </v-col>
-      <v-col cols="7" align-self="center">
-        <span class="date-time warning--text font-weight-bold text-h2">{{
-          time
-        }}</span>
-      </v-col>
-      <v-col cols="1" align-self="center">
-        <v-icon
-          class="date-time warning--text font-weight-bold text-h3 align-center">
-          mdi-alert-rhombus
-        </v-icon>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12">
-        <v-chip
-          v-for="(chip) in demoChips"
-          :key="chip.id"
-          class="mr-2 text--text"
-          color="gray300"
-        >
-          {{ chip.title }}
-        </v-chip>
-      </v-col>
-    </v-row>
-  </v-card>
+  <div>
+    <v-card v-for="(card, index) in cardDatas" id="`card_${index}`" :key="index" class="pa-4 pt-2" :style="cardPosition">
+      <v-row no-gutters class="pa-0 pb-2">
+        <v-col
+          cols="9"
+          class="grabbingArea pa-0"
+          @mousedown="onMouseDown"
+          @mouseup="onMouseUp"
+          @dragstart="onDragStart"
+        /></v-col>
+        <v-col cols="3" class="px-0">
+          <v-btn small icon class="pa-0">
+            <v-icon class="text-right" color="gray400" @click="editCard"> mdi-pencil </v-icon>
+          </v-btn>
+          <v-btn small icon class="pa-0" @click="deleteCard">
+            <v-icon class="text-right" color="gray400"> mdi-close </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-divider class="mb-2" />
+      <v-row no-gutters class="ma-0 pa-0">
+        <v-col cols="4" align-self="center">
+          <p class="date-time warning--text ma-0 font-weight-bold">
+            {{ card.due }}
+          </p>
+        </v-col>
+        <v-col cols="7" align-self="center">
+          <span class="date-time warning--text font-weight-bold text-h2">{{
+            card.time
+          }}</span>
+        </v-col>
+        <v-col cols="1" align-self="center">
+          <v-icon
+            class="date-time warning--text font-weight-bold text-h3 align-center">
+            mdi-alert-rhombus
+          </v-icon>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-chip
+            v-for="(chip) in card.chips"
+            :key="chip.id"
+            class="mr-2 text--text"
+            color="gray300"
+          >
+            {{ chip.title }}
+          </v-chip>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -60,20 +62,91 @@ export default defineComponent({
     const date = ref<string>('2021.12.31')
     const time = ref<string>('23:59')
 
-    // あとで消す
-    const demoChips = [
+    const cardDatas = [
       {
         id: 1,
-        title: '在庫整理'
+        due: '2021.12.31',
+        time: '23:59',
+        isDead: true,
+        createdAt: '',
+        updatedAt: '',
+        isDone: false,
+        chips: [{
+          id: 1,
+          title: '在庫整理'
+        },
+        {
+          id: 2,
+          title: '電話'
+        },
+        {
+          id: 3,
+          title: '吉田さん'
+        }]
       },
       {
         id: 2,
-        title: '電話'
+        due: '2021.03.31',
+        time: '20:30',
+        isDead: false,
+        createdAt: '',
+        updatedAt: '',
+        isDone: false,
+        chips: [{
+          id: 1,
+          title: '在庫整理'
+        },
+        {
+          id: 2,
+          title: '電話'
+        },
+        {
+          id: 3,
+          title: '吉田さん'
+        }]
       },
       {
         id: 3,
-        title: '吉田さん'
-      }
+        due: '2021.04.01',
+        time: '23:59',
+        isDead: false,
+        createdAt: '',
+        updatedAt: '',
+        isDone: false,
+        chips: [{
+          id: 1,
+          title: '在庫整理'
+        },
+        {
+          id: 2,
+          title: '電話'
+        },
+        {
+          id: 3,
+          title: '吉田さん'
+        }]
+      },
+      {
+        id: 4,
+        due: '2021.10.11',
+        time: '23:59',
+        isDead: true,
+        createdAt: '',
+        updatedAt: '',
+        isDone: false,
+        chips: [{
+          id: 1,
+          title: '在庫整理'
+        },
+        {
+          id: 2,
+          title: '電話'
+        },
+        {
+          id: 3,
+          title: '吉田さん'
+        }]
+      },
     ]
 
     // カーソル表示
@@ -118,13 +191,13 @@ export default defineComponent({
     return {
       date,
       time,
-      demoChips,
       editCard,
       deleteCard,
       onMouseDown,
       onMouseUp,
       cardPosition,
-      onDragStart
+      onDragStart,
+      cardDatas
     }
   }
 })
