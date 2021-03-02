cbeb742211356a08ca9cb2e97c89e12f19d89c10
JOKOJOKO405 <biz.kj405@gmail.com>
2021-03-02 11:18:34 +0900

    レイアウト追加・変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="pa-0 ma-0">
       <nuxt />
     </v-main>
   </v-app>
---
+++ layouts/user.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-main class="pa-0 ma-0">
+      <v-app-bar color="white" elevation="1">
+        <v-avatar color="accent" size="32">
+          <v-icon dark> mdi-account-circle </v-icon>
+        </v-avatar>
+        <v-toolbar-title class="ml-2">{{ userName }}</v-toolbar-title>
+        <v-spacer />
+        <v-btn icon color="warning">
+          <v-icon>mdi-clock-alert</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-cog</v-icon>
+        </v-btn>
+        <v-btn icon class="ml-4">
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const userName = ref<string>('えいじ')
+
+    return {
+      userName
+    }
+  }
+})
+</script>
